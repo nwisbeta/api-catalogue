41f772bc7b5d9394a75738dd210c6ef2b7ba36aa
Huw Angle <Huw.Angle@wales.nhs.uk>
2020-06-02 12:18:57 +0100

    tidy up and add error handling

--- .github/actions/api-catalogue-processor/script.js
+++ .github/actions/api-catalogue-processor/script.js
@@ -1,63 +1,57 @@
-const fs = require('fs');
+#!/usr/bin/env node
+
+const fs = require('fs')
 const crypto = require('crypto');
-const https = require('https');
-const { abort } = require('process');
+const https = require('https')
 
-const args = process.argv.slice(2);
+const cli = initCli("context", "apimInstance", "apimAccessKey")
 
-if (args.length !== 3){
-    console.log(`usage: node ${process.argv[1]} context apimInstance apimAccessKey`)
-    process.exit()
-}
-   
+const sasToken = createSharedAccessToken("integration", cli.args["apimAccessKey"], 2)
 
-const context = args[0]
-const apimInstance = args[1]
-const apimAccessKey = args[2]
+process.chdir(cli.args["context"]);
 
-process.chdir(context);
-
-const sasToken = createSharedAccessToken("integration", apimAccessKey, 2 )
-
-console.log(process.cwd())
-
-fs.readdir('./', { withFileTypes : true }, (err, dirents) => {
+fs.readdir('./', { withFileTypes: true }, (err, dirents) => {
 
     if (err) throw err
 
-    let httpRequests = []
+    let httpCommands = []
 
-    for(const dirent of dirents){
+    for (const dirent of dirents) {
 
-        let httpRequest = null
-        if (dirent.isFile() && (httpRequest = extractHttpRequest(dirent, apimInstance, sasToken))) {
-            httpRequests.push(httpRequest)
+        let httpCommand = null
+        if (dirent.isFile() && (httpCommand = extractHttpCommand(dirent, cli.args["apimInstance"], sasToken))) {
+            httpCommands.push(httpCommand)
         }
     }
 
-    httpRequests.sort(methodPrecedence)
+    httpCommands.sort(methodPrecedence)
 
-    for(const request of httpRequests){
-        const r = https.request(request.options, (res) => { 
-            console.log(`PATH: ${request.options.path}\nMETHOD: ${request.options.method}\nRESPONSE: ${res.statusCode}`) 
+    for (const command of httpCommands) {
+
+        const request = https.request(command.options, (response) => {
+            console.log(`PATH: ${command.options.path}\nMETHOD: ${command.options.method}\nRESPONSE: ${response.statusCode}`)
         })
 
-        r.write(request.data)
-        r.on('error', logError)
-        r.end()
+        request.write(command.data)
+        request.on('error', cli.logError)
+        request.end()
+
+        console.log(command.options)
+        console.log(command.data)
+        process.exit()
     }
 
 })
 
 
 
-function createSharedAccessToken(apimUid, apimAccessKey, validForDays = 1) { 
+function createSharedAccessToken(apimUid, apimAccessKey, validForDays = 1) {
 
     const x = new Date()
     x.setDate(x.getDate() + validForDays)
     let expiry = x.toISOString().replace("Z", "0000Z")
 
-    const signature = crypto.createHmac('sha512', apimAccessKey).update(`${apimUid}\n${expiry}`).digest('base64'); 
+    const signature = crypto.createHmac('sha512', apimAccessKey).update(`${apimUid}\n${expiry}`).digest('base64');
     const sasToken = `SharedAccessSignature uid=${apimUid}&ex=${expiry}&sn=${signature}`;
 
     return sasToken;
@@ -68,26 +62,33 @@ function createSharedAccessToken(apimUid, apimAccessKey, validForDays = 1) {
 //Regex for API template files, e.g. "my-awesomme-api-name.PATCH.json"     
 const fileRegex = /([^\.]+)\.(GET|PUT|POST|PATCH|DELETE)+\.json$/
 
-function extractHttpRequest(dirent, instanceName, sasToken) {
+function extractHttpCommand(dirent, instanceName, sasToken) {
 
     const arr = fileRegex.exec(dirent.name)
 
     if (arr === null) return null
 
     const apiName = arr[1]
-    const method = arr[2]     
-    const data = JSON.stringify(JSON.parse( fs.readFileSync(dirent.name, 'utf8')))
+    const method = arr[2]
+
+    let data = null;
+    try {
+        data = JSON.stringify(JSON.parse(fs.readFileSync(dirent.name, 'utf8')))
+    } catch (err) {
+        cli.logError(`Error parsing JSON from file (${dirent.name}):\n\t${err}`)
+        process.exit()
+    }    
 
     return {
-        options: { 
+        options: {
             host: `${instanceName}.management.azure-api.net`,
-            path: `/subscriptions/x/resourceGroups/x/providers/Microsoft.ApiManagement/service/x/apis/${apiName}?api-version=2019-12-01`, 
-            method, 
+            path: `/subscriptions/x/resourceGroups/x/providers/Microsoft.ApiManagement/service/x/apis/${apiName}?api-version=2019-12-01`,
+            method,
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": sasToken
             }
-        }, 
+        },
         data
     };
 }
@@ -98,11 +99,44 @@ function methodPrecedence(req1, req2) {
     m2 = req2.options.method
 
     if ((m1 === "PUT" && m2 !== "PUT") || (m1 !== "PUT" && m2 === "PUT")) {
-            return m1 === "PUT" ? -1 : 1
+        return m1 === "PUT" ? -1 : 1
     }
     else return 0
 }
 
-function logError(err){
-    console.error(err)
+
+function initCli() {
+    const argNames = Array.from(arguments)
+    const args = {}
+    const cliArgs = process.argv.slice(2)
+    if (cliArgs.length !== argNames.length) {
+        printUsage(argNames)
+        process.exit()
+    }
+    else {
+        let i = 0;
+        for( name of argNames){
+            args[name] = cliArgs[i++]
+        }
+    }
+
+    function logError(err) {
+        console.error(err)
+    }
+    
+    function printUsage(namedArgs) {  
+        const path = require('path')    
+
+        scriptName =  path.basename(process.argv[1])
+        console.log(`Usage: node ${scriptName} ${namedArgs.join(" ")}`)
+    }
+
+    return {
+        args,
+        logError,
+        printUsage
+    }
 }
+
+
+
